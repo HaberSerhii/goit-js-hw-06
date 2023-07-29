d0b41f034d1a0f6075cb18b5615611602ac42c13
Serhii Haber <haberserhii@icloud.com>
2023-07-29 18:06:57 +0300

    update

--- js/task-02.js
+++ js/task-02.js
@@ -1,17 +1,33 @@
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
+
+// const ingredientsList = document.getElementById("ingredients");
+
+// ingredients.forEach((ingredient) => {
+//   const listItem = document.createElement("li");
+//   listItem.textContent = ingredient;
+//   listItem.classList.add("item");
+//   ingredientsList.appendChild(listItem);
+// });
+
 const ingredients = [
   "Potatoes",
   "Mushrooms",
   "Garlic",
-  "Tomatos",
+  "Tomatoes",
   "Herbs",
   "Condiments",
 ];
 
-const ingredientsList = document.getElementById("ingredients");
+const ingredientsList = document.querySelector("#ingredients");
 
-ingredients.forEach((ingredient) => {
-  const listItem = document.createElement("li");
-  listItem.textContent = ingredient;
-  listItem.classList.add("item");
-  ingredientsList.appendChild(listItem);
-});
+const listItems = ingredients.map(
+  (ingredient) => `<li class="item">${ingredient}</li>`
+);
+ingredientsList.innerHTML = listItems.join("");
